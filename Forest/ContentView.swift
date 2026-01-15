1684387132c82e84724e9d6c02fb04583c9853b8
ricwo <r@cogr.am>
2026-01-15 11:20:49 +0100

    fix: adjust traffic lights y position

--- Forest/ContentView.swift
+++ Forest/ContentView.swift
@@ -33,7 +33,7 @@ struct ContentView: View {
                 for buttonType in buttons {
                     if let button = window.standardWindowButton(buttonType) {
                         var frame = button.frame
-                        frame.origin.y = 16  // Center in 52pt header
+                        frame.origin.y = -12  // Move down to center in header
                         button.setFrameOrigin(frame.origin)
                     }
                 }
