df171f4593d6c7dd5ceb6f47e47b7ab477206ce2
ricwo <r@cogr.am>
2026-01-15 11:09:55 +0100

    remove window configuration

--- Forest/ForestApp.swift
+++ Forest/ForestApp.swift
@@ -15,7 +15,6 @@ struct ForestApp: App {
             ContentView()
                 .environment(appState)
                 .environment(updateService)
-                .background(WindowAccessor())
                 .onAppear {
                     logService.info("Forest app launched", category: "App")
                     updateService.startPeriodicChecks()
@@ -63,22 +62,4 @@ struct ForestApp: App {
         }
         settingsService.appearanceRefreshTrigger += 1
     }
-}
-
-// MARK: - Window Configuration
-
-struct WindowAccessor: NSViewRepresentable {
-    func makeNSView(context: Context) -> NSView {
-        let view = NSView()
-        DispatchQueue.main.async {
-            if let window = view.window {
-                window.title = "forest"
-                window.titleVisibility = .visible
-                window.toolbarStyle = .unified
-            }
-        }
-        return view
-    }
-
-    func updateNSView(_ nsView: NSView, context: Context) {}
 }
